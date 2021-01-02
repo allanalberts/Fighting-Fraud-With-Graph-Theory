bc9dc0ea2d73194548b560b96f8014bbb9db0a95
allanalberts <allan.alberts@gmail.com>
2021-01-01 21:20:26 -0800

    add graph vis functions

--- src/visualizations.py
+++ src/visualizations.py
@@ -2,6 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
+import networkx as nx
+import nxpd
+
 def plot_timeline(df, title):
     """ Plots positive and negative user rating counts over time
     Inputs:
@@ -18,3 +21,20 @@ def plot_timeline(df, title):
     ax.set_title(title)
     plt.tight_layout()
 
+def user_graph(user, bitcoin_G):
+    """ Returns a graph of user's given and received ratings
+    Input:
+        user: int
+        bitcoin_G: graph containing user node
+    """
+    return nx.ego_graph(bitcoin_G, user, radius=1)
+
+def add_user_to_graph(existing_graph, new_user, bitcoin_g):
+    """ Returns a new graph object that now also includes the user's
+    given and received ratings.
+    Input: 
+        existing_graph: graph object
+        new_user: int
+        bitcoin_g: graph containing new user node
+    """
+    return nx.compose(existing_graph, nx.ego_graph(bitcoin_g, new_user, radius=1))
