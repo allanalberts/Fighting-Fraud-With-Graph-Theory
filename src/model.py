7b09a96115abce4340ed177beef0471922a082a8
allanalberts <allan.alberts@gmail.com>
2021-01-19 20:52:20 -0800

    updated velocity function

--- src/model.py
+++ src/model.py
@@ -147,41 +147,80 @@ def iteration_feature_creation(marketplace, bitcoin_df):
         df.at[(i,'ego_num_cliques')] = arr[19]
     return df
 
-def date_velocity(bitcoin_df, user, rate_date, vel_parm, user_type):
-    df = bitcoin_df.copy()
-    from_date = str(pd.Timestamp(rate_date) - pd.offsets.Hour(vel_parm))
-    vel_neg, vel_all = \
-    df[(df[user_type]==user) & (df['date'] < rate_date) & (df['date'] >= from_date)]['class'].agg(['sum', 'count'])
-    vel_pos = vel_all - vel_neg
-    A = np.array([vel_neg, vel_pos, vel_all])
-    A[np.isnan(A)] = 0
-    return A
+# def date_velocity(bitcoin_df, user, rate_date, vel_parm, user_type):
+#     df = bitcoin_df.copy()
+#     from_date = str(pd.Timestamp(rate_date) - pd.offsets.Hour(vel_parm))
+#     vel_neg, vel_all = \
+#     df[(df[user_type]==user) & (df['date'] < rate_date) & (df['date'] >= from_date)]['class'].agg(['sum', 'count'])
+#     vel_pos = vel_all - vel_neg
+#     A = np.array([vel_neg, vel_pos, vel_all])
+#     A[np.isnan(A)] = 0
+#     return A
 
-def feature_iteration_date_velocity(bitcoin_df):
+def current_velocity(bitcoin_df, user, rate_date, vel_parm):
+    """ Returns the current velocity associated with the user
+    Input:
+        bitcoin_df: dataframe of bitcoin activity
+        user: int - from current record user
+        rate_date: date - from current record
+        vel_parm: int - the velocity timeframe in minutes
+    """
     df = bitcoin_df.copy()
-    df = df[['ratee', 'rater', 'rating','date','class']]
+    from_date = str(pd.Timestamp(rate_date) - pd.offsets.Minute(vel_parm))
+    velocity_current = \
+    df[((df['rater']==user) | (df['ratee']==user)) & (df['date'] < rate_date) & (df['date'] >= from_date)]['rating'].count()
+    np.nan_to_num(velocity_current, copy=False)
+    return velocity_current
+
+def max_velocity(bitcoin_df, user, rate_date):
+    """ Returns the maximum velocity associated with the user
+    Input:
+        bitcoin_df: dataframe of bitcoin activity
+        user: int - from current record user
+        rate_date: date - from current record
+    """
+    df = bitcoin_df.copy()
+    velocity_max = df[((df['rater']==user) | (df['ratee']==user)) & (df['date'] < rate_date)]['velocity_current'].max()
+    return velocity_max
+
+def feature_iteration_velocity(bitcoin_df):
+    """ Returns dataframe with updated velocity transaction count metrics (current and max)
+    Input:
+        bitcoin_df: dataframe of bitcoin transactions
+    """
+    df = bitcoin_df.copy()
+    df = df[['ratee', 'rater', 'rating','date']]
     for i, row in df.iterrows():
         user = row['ratee']
         rate_date = row['date']
-        vel_24_in_neg, vel_24_in_pos, vel_24_in_all = date_velocity(df, user, rate_date, vel_parm=24, user_type="ratee")
-        vel_24_out_neg, vel_24_out_pos, vel_24_out_all = date_velocity(df, user, rate_date, vel_parm=24, user_type="rater")
-        vel_48_in_neg, vel_48_in_pos, vel_48_in_all = date_velocity(df, user, rate_date, vel_parm=48, user_type="ratee")
-        vel_48_out_neg, vel_48_out_pos, vel_48_out_all = date_velocity(df, user, rate_date, vel_parm=48, user_type="rater")
-        df.at[(i,'vel_24_in_neg')] = vel_24_in_neg
-        df.at[(i,'vel_48_in_neg')] = vel_48_in_neg
-    df.drop(['class'], axis=1)
+        velocity_current = current_velocity(df, user, rate_date, vel_parm=10)
+        df.at[(i,'velocity_current')] = velocity_current
+        velocity_max = max_velocity(df, user, rate_date)
+        df.at[(i,'velocity_max')] = velocity_max
     return df
 
+# def feature_iteration_date_velocity(bitcoin_df):
+#     df = bitcoin_df.copy()
+#     df = df[['ratee', 'rater', 'rating','date','class']]
+#     for i, row in df.iterrows():
+#         user = row['ratee']
+#         rate_date = row['date']
+#         vel_24_in_neg, vel_24_in_pos, vel_24_in_all = date_velocity(df, user, rate_date, vel_parm=24, user_type="ratee")
+#         vel_24_out_neg, vel_24_out_pos, vel_24_out_all = date_velocity(df, user, rate_date, vel_parm=24, user_type="rater")
+#         vel_48_in_neg, vel_48_in_pos, vel_48_in_all = date_velocity(df, user, rate_date, vel_parm=48, user_type="ratee")
+#         vel_48_out_neg, vel_48_out_pos, vel_48_out_all = date_velocity(df, user, rate_date, vel_parm=48, user_type="rater")
+#         df.at[(i,'vel_24_in_neg')] = vel_24_in_neg
+#         df.at[(i,'vel_48_in_neg')] = vel_48_in_neg
+#     df.drop(['class'], axis=1)
+#     return df
 if __name__ == '__main__':
-    df_features_otc = iteration_feature_creation(marketplace='otc', bitcoin_df=otc_df)
-    df_features_otc.to_csv('../data/df_features_otc.csv', index=False)
 
-    df_otc_vd = feature_iteration_date_velocity(otc_df)
-    df_otc_vd.to_csv('../data/df_otc_vd.csv', index=False)
+    otc_df = h.load_bitcoin_edge_data('../data/soc-sign-bitcoinotc.csv.gz')
+    # df_features_otc = iteration_feature_creation(marketplace='otc', bitcoin_df=otc_df)
+    # df_features_otc.to_csv('../data/df_features_otc.csv', index=False)
 
-    df_features_alpha = iteration_feature_creation(marketplace='alpha', bitcoin_df=alpha_df)
-    df_features_alpha.to_csv('../data/df_features_alpha2.csv', index=False)
+    df_otc_vel = feature_iteration_velocity(otc_df)
+    df_otc_vel.to_csv('../data/df_otc_velocity.csv', index=False)
 
-    df_alpha_vd = feature_iteration_date_velocity(alpha_df)
-    df_alpha_vd.to_csv('../data/df_alpha_vd.csv', index=False)
-
+    # df_features_alpha = iteration_feature_creation(marketplace='alpha', bitcoin_df=alpha_df)
+    # df_features_alpha.to_csv('../data/df_features_alpha2.csv', index=False)
